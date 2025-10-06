25994c77c09d559e4ffc189ef4b3f1fab81329c6
Juan Llinares Mauri <78471931+jlm109-ua@users.noreply.github.com>
2025-10-06 17:20:44 +0200

    recetas

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Suspense } from "react"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "Pizzas :p",
+  title: "Recetas :p",
   description: "de Jimy y Nhoa",
   icons: {
     icon: "/logo.svg",
